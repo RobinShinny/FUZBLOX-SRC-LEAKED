5229e2bb8dfbeab6caf829f818957a434b2c15d0
ModlyCheeser <160442862+RobinShinny@users.noreply.github.com>
2024-10-06 19:13:15 -0500

    yes

---
+++ value.js
@@ -0,0 +1,8 @@
+function dwnload() {
+
+window.location.href = 'https://github.com/RobinShinny/FUZBLOX-SRC-LEAKED/raw/refs/heads/main/FUZBLOXSRC.zip';
+
+
+	console.log('this might solve the errors!');
+
+	}
